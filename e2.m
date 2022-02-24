35ce8b5584d2a38bb326869ddd7912eb1c7af4ae
Kevin Zeledón <magneticok@gmail.com>
2022-02-23 21:25:11 -0600

    Traslapando graficos

--- e2.m
+++ e2.m
@@ -1,21 +1,28 @@
 n = 0:74;
 
 f = 0.01;
-y = sin(3*pi*f*n);
+y1 = sin(3*pi*f*n);
 subplot(2,2,1);
-stem(n,y);
+stem(n,y1);
 
 f = 0.02;
-y = sin(3*pi*f*n);
+y2 = sin(3*pi*f*n);
 subplot(2,2,2);
-stem(n,y);
+stem(n,y2);
 
 f = 0.05;
-y = sin(3*pi*f*n);
+y3 = sin(3*pi*f*n);
 subplot(2,2,3);
-stem(n,y);
+stem(n,y3);
 
-f = 0.01;
-y = sin(3*pi*f*n);
+f = 0.1;
+y4 = sin(3*pi*f*n);
 subplot(2,2,4);
-stem(n,y);
+stem(n,y4);
+
+figure;
+hold on;
+stem(n, y1);
+stem(n, y2);
+stem(n, y3);
+stem(n, y4);
